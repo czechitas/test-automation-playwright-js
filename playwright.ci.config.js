b79b7a19eeed8df309c55ad2e7b6a79c84acd9c5
Zaneta <zaneta@alfred.is>
2024-12-17 21:33:00 +0100

    use specific config for CI

--- playwright.ci.config.js
+++ playwright.ci.config.js
@@ -1,39 +1,38 @@
 // @ts-check
-require('dotenv').config();
-
-const { defineConfig, devices } = require('@playwright/test');
+require("dotenv").config({ path: ".env.testing" });
+const { defineConfig, devices } = require("@playwright/test");
 const { BASE_URL } = process.env;
 
 /**
  * @see https://playwright.dev/docs/test-configuration
  */
 module.exports = defineConfig({
-  testDir: './src/tests',
+  testDir: "./src/examples",
   // testDir: './src/examples',
   /* Run tests in files in parallel */
   fullyParallel: false,
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
+  retries: process.env.CI ? 1 : 0,
   /* Opt out of parallel tests on CI. */
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: 'html',
+  reporter: [["html", { outputFolder: "playwright-report", open: "never" }]],
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
     baseURL: BASE_URL,
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry',
+    trace: "on-first-retry",
   },
 
   /* Configure projects for major browsers */
   projects: [
     {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      name: "chromium",
+      use: { ...devices["Desktop Chrome"] },
     },
 
     // {
